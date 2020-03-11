f737b8f5ca34e7bad043c41594279aefde619d76
Onfanta <976294990@qq.com>
2020-03-11 15:34:56 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main() {
+	chos := [] string {"Pool Test”,”Ec Test“,”User Test","Rgw Test","Test All","exit"}
+	var cho int
+	fmt.Printf("function:")
+	for i:=0;i< len(chos);i++  {
+		fmt.Printf(chos[i])
+	}
+	fmt.Printf("plz input option u want[1-6] :")
+	fmt.Scanf(cho)
+	if cho == 5  {
+		fmt.Printf("processing...")
+		PoolTest()
+		EcTest()
+		UserTest()
+		RgwTest()
+	}else if cho < 5 {
+		switch cho {
+		case 1:
+			fmt.Println("PoolTest is started")
+			PoolTest()
+		case 2:
+			fmt.Println("EcTest is started")
+			EcTest()
+		case 3:
+			fmt.Println("UserTest is started")
+			UserTest()
+		case 4:
+			fmt.Println("RGWTest is started")
+			RgwTest()
+		}
+	}else if cho == 6 {
+		fmt.Println("Exiting...")
+
+	}else {
+		fmt.Println("type err,plz input again...")
+	}
+}
